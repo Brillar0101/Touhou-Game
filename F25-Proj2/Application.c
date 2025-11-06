41fef1839aa0a23280534198d8c1e0b71b2bc628
hungryshark7 <barakaelilawuo0101@gmail.com>
2025-11-06 10:30:07 -0500

    Implemented 3 seconds timer

--- F25-Proj2/Application.c
+++ F25-Proj2/Application.c
@@ -9,22 +9,60 @@
 // Set initial values
 Application Application_construct() {
     Application app;
-    app.hasDrawnTitle = false;
+
+    // Starting application on title screen
+    app.currentScreen = TITLE_SCREEN;
+    app.firstCall = true;
+
+    // Creating a 3 second timer
+    app.titleTimer = SWTimer_construct(3000);
+    app.timerStarted = false;
+
     return app;
 }
 
 
 void Application_loop(Application* app, HAL* hal, Graphics_Context* g_sContext_p) {
-    if (!app->hasDrawnTitle) {
-        drawTitleScreen(g_sContext_p);
-        app->hasDrawnTitle = true;
-    }
 
     // Refreshing buttons for non-blocking checks
     HAL_refresh(hal);
+
+
+    if (app->currentScreen == TITLE_SCREEN) {
+
+            // Draws title of the screen o first call
+            if (app->firstCall) {
+                drawTitleScreen(g_sContext_p);
+
+                // Then starts a 3 seconds timer
+                SWTimer_start(&app->titleTimer);
+                app->timerStarted = true;
+
+                app->firstCall = false;
+            }
+
+            // Checks if 3 seconds has passed
+            if (app->timerStarted && SWTimer_expired(&app->titleTimer)) {
+                // Moves to Menu Screen
+                app->currentScreen = MENU_SCREEN;
+                app->firstCall = true;
+            }
+        }
+
+        else if (app->currentScreen == MENU_SCREEN) {
+
+            // Drawing the menu on the first call
+            if (app->firstCall) {
+                drawMenuScreen(g_sContext_p);
+                app->firstCall = false;
+            }
+
+            // Going to add buttons functionality later
+        }
 }
 
 // Drawing the title screen
+
 void drawTitleScreen(Graphics_Context* g_sContext_p) {
     // Clearing the screen
     Graphics_clearDisplay(g_sContext_p);
@@ -39,4 +77,22 @@ void drawTitleScreen(Graphics_Context* g_sContext_p) {
 
     // The name of the student
     Graphics_drawString(g_sContext_p, (int8_t*)"Baraka Lawuo", -1, 20, 95, true);
+}
+
+
+// Drawing the menu screen
+void drawMenuScreen(Graphics_Context* g_sContext_p) {
+    // Clearing the screen
+    Graphics_clearDisplay(g_sContext_p);
+
+    // Setting the font
+    Graphics_setFont(g_sContext_p, &g_sFontCmtt12);
+
+    // Drawing menu title
+    Graphics_drawString(g_sContext_p, (int8_t*)"MENU", -1, 45, 10, true);
+
+    // Drawing menu options with curosr on the first option ( Will fix this later)
+    Graphics_drawString(g_sContext_p, (int8_t*)"> Play", -1, 10, 40, true);
+    Graphics_drawString(g_sContext_p, (int8_t*)"  Instructions", -1, 10, 55, true);
+    Graphics_drawString(g_sContext_p, (int8_t*)"  High Scores", -1, 10, 70, true);
 }
--- F25-Proj2/Application.h
+++ F25-Proj2/Application.h
@@ -6,18 +6,33 @@
  * HW9
  * Implemented features so far
  *       - Title Screen
+ *       - Auto transition after 3 seconds
  *
  */
 
 #ifndef APPLICATION_H_
 #define APPLICATION_H_
 
+
 #include <HAL/HAL.h>
 #include <ti/grlib/grlib.h>
+#include <HAL/Timer.h>
+
+// Enum for screen states
+enum _ScreenState {
+    TITLE_SCREEN,
+    MENU_SCREEN
+};
+typedef enum _ScreenState ScreenState;
+
 
 // Struct showing what is show on the screen
 struct _Application {
-    bool hasDrawnTitle;
+
+    ScreenState currentScreen;   // Which screen user is on
+    bool firstCall;              // First time drawing on screen ?
+    SWTimer titleTimer;          // 3 sec timer
+    bool timerStarted;           // Timer started yet ?
 };
 typedef struct _Application Application;
 
@@ -27,7 +42,8 @@ Application Application_construct();
 // Main loop
 void Application_loop(Application* app, HAL* hal, Graphics_Context* g_sContext_p);
 
-// Drawing the title screen
+// Drawing functions
 void drawTitleScreen(Graphics_Context* g_sContext_p);
+void drawMenuScreen(Graphics_Context* g_sContext_p);
 
 #endif /* APPLICATION_H_ */
